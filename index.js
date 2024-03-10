b4487dfa22560e17278ad18a5d540db7445fecdc
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-03-10 12:29:46 -0400

    kinda works but still throwin an error

--- index.js
+++ index.js
@@ -2,27 +2,26 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
-let direction = null; 
-let x = 100; 
-let y = 250; 
-move(character).to(100, 250)
 
-setInterval(function(){ 
+function handleDirectionChange(){
+    if(direction === null){
+        character.src = 'assets/green-character/static.gif'
+    }
     if(direction === 'west'){
-        x = x - 1
+        character.src = 'assets/green-character/west.gif'
     }
     if(direction === 'north'){
-        y = y + 1
+        character.src = 'assets/green-character/north.gif'
     }
     if(direction === 'east'){
-        x = x + 1
+        character.src = 'assets/green-character/east.gif'
     }
     if(direction === 'south'){
-        y = y - 1
+        character.src = 'assets/green-character/south.gif'
     }
-    character.style.left = x + 'px'
-    character.style.bottom = y + 'px'
-}, 1)
+}
+
+move(character).withArrowKeys(100, 250, handleDirectionChange)
 
 document.addEventListener('keydown', function(e){
     if(e.repeat) return;
